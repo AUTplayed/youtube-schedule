af47d3f378d9e330c25e69ea72b86341e00e485e
AUTplayed <fips.hem@gmail.com>
2017-03-16 00:15:03 +0100

    now persisting tokens

--- google.js
+++ google.js
@@ -1,24 +1,34 @@
 var google = require('googleapis');
 var readline = require('readline');
+var fs = require("fs");
 
-module.exports = function(scope,callback){
-	var OAuth2 = google.auth.OAuth2;
-	var oauth2Client = new OAuth2(
-	  process.env.CLIENTID,
-	  process.env.CLIENTSECRET,
-	  process.env.REDIRECTURL
-	);
+module.exports = function (scope, callback) {
+  var OAuth2 = google.auth.OAuth2;
+  var oauth2Client = new OAuth2(
+    process.env.CLIENTID,
+    process.env.CLIENTSECRET,
+    process.env.REDIRECTURL
+  );
 
+  fs.readFile(process.env.TOKENPATH, function (err, token) {
+    if (err) {
+      getNewToken(oauth2Client, scope, function (oa2c) {
+        google.options({
+          auth: oa2c
+        });
+        callback(google);
+      });
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      google.options({
+        auth: oa2c
+      });
+      callback(google);
+    }
+  });
+};
 
-	getNewToken(oauth2Client,scope,function(oa2c){
-		google.options({
-		  auth: oa2c
-		});
-		callback(google);
-	});
-}
-
-function getNewToken(oauth2Client,scope, callback) {
+function getNewToken(oauth2Client, scope, callback) {
   var authUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: scope
@@ -28,14 +38,14 @@ function getNewToken(oauth2Client,scope, callback) {
     input: process.stdin,
     output: process.stdout
   });
-  rl.question('Enter the code from that page here: ', function(code) {
+  rl.question('Enter the code from that page here: ', function (code) {
     rl.close();
-    oauth2Client.getToken(code, function(err, token) {
+    oauth2Client.getToken(code, function (err, token) {
       if (err) {
         console.log('Error while trying to retrieve access token', err);
         return;
       }
-      console.log(token);
+      fs.writeFile(process.env.TOKENPATH, JSON.stringify(token));
       oauth2Client.credentials = token;
       callback(oauth2Client);
     });
